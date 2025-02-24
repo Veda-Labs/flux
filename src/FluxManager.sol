8d16b477ed579a029a2d0ca533259c0955611687
crispymangoes <crispymangoes@gmail.com>
2025-02-24 09:24:09 -0800

    Setup flux contracts

---
+++ src/FluxManager.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {Auth, Authority} from "@solmate/auth/Auth.sol";
+import {IPausable} from "@boring-vault/interfaces/IPausable.sol";
+import {BoringVault} from "@boring-vault/base/BoringVault.sol";
+import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+/// @notice Manager used for Flux Boring Vaults
+///
+/// @dev
+/// This contract is responsible for gating calls the boring vault can make
+/// and for abstratcing the total assets calculation down to a flux function.
+abstract contract FluxManager is Auth, IPausable {
+    using FixedPointMathLib for uint256;
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         STATE                              */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /**
+     * @notice Used to pause calls to `FluxManager`.
+     */
+    bool public isPaused;
+    uint8 public exchangeRateDecimals;
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         ERRORS                             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    error FluxManager__Paused();
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         EVENTS                             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    event Paused();
+    event Unpaused();
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       IMMUTABLES                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    BoringVault internal immutable boringVault;
+    ERC20 public immutable token0;
+    ERC20 public immutable token1;
+    uint8 internal immutable decimals0;
+    uint8 internal immutable decimals1;
+    uint8 internal immutable decimalsBoring;
+
+    constructor(address _owner, address _boringVault, address _token0, address _token1)
+        Auth(_owner, Authority(address(0)))
+    {
+        boringVault = BoringVault(payable(_boringVault));
+        token0 = ERC20(_token0);
+        token1 = ERC20(_token1);
+        decimals0 = token0.decimals();
+        decimals1 = token1.decimals();
+        decimalsBoring = boringVault.decimals();
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                    ADMIN FUNCTIONS                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Pause this contract, which prevents future calls to `FluxManager`.
+    /// @dev Callable by MULTISIG_ROLE.
+    function pause() external requiresAuth {
+        isPaused = true;
+        emit Paused();
+    }
+
+    /// @notice Unpause this contract, which allows future calls to `FluxManager`.
+    /// @dev Callable by MULTISIG_ROLE.
+    function unpause() external requiresAuth {
+        isPaused = false;
+        emit Unpaused();
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                    FLUX ACCOUNTING                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function refreshInternalFluxAccounting() external requiresAuth {
+        _refreshInternalFluxAccounting();
+    }
+
+    function refreshExternalFluxAccounting() external requiresAuth {
+        _refreshExternalFluxAccounting();
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       FLUX VIEW                            */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function totalAssets(uint256 exchangeRate, bool quoteIn0Or1) public view returns (uint256 assets) {
+        (uint256 token0Assets, uint256 token1Assets) = _totalAssets(exchangeRate);
+        if (quoteIn0Or1) {
+            // Return totalAssets in token0
+            uint256 converted = token1Assets * (10 ** decimals0);
+            converted = converted.mulDivDown(10 ** exchangeRateDecimals, exchangeRate);
+            converted /= 10 ** decimals1;
+            assets = token0Assets + converted;
+        } else {
+            // Return totalASsets in token1
+            uint256 converted = token0Assets * (10 ** decimals1);
+            converted = converted.mulDivDown(exchangeRate, 10 ** exchangeRateDecimals);
+            converted /= 10 ** decimals0;
+            assets = token1Assets + converted;
+        }
+    }
+
+    function totalAssets(uint256 exchangeRate) external view returns (uint256 token0Assets, uint256 token1Assets) {
+        return _totalAssets(exchangeRate);
+    }
+
+    function getRate(uint256 exchangeRate, bool quoteIn0Or1) public view returns (uint256) {
+        uint256 ts = boringVault.totalSupply();
+        uint256 ta = totalAssets(exchangeRate, quoteIn0Or1);
+        return ta.mulDivDown(10 ** decimalsBoring, ts);
+    }
+
+    function getRateSafe(uint256 exchangeRate, bool quoteIn0Or1) external view returns (uint256) {
+        if (isPaused) revert FluxManager__Paused();
+        return getRate(exchangeRate, quoteIn0Or1);
+    }
+
+    function getRate(uint256 exchangeRate) public view returns (uint256 token0PerShare, uint256 token1PerShare) {
+        uint256 ts = boringVault.totalSupply();
+        (uint256 ta0, uint256 ta1) = _totalAssets(exchangeRate);
+        token0PerShare = ta0.mulDivDown(10 ** decimalsBoring, ts);
+        token1PerShare = ta1.mulDivDown(10 ** decimalsBoring, ts);
+    }
+
+    function getRateSafe(uint256 exchangeRate) external view returns (uint256 token0PerShare, uint256 token1PerShare) {
+        if (isPaused) revert FluxManager__Paused();
+        return getRate(exchangeRate);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                   ABSTRACT FUNCTIONS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Refresh internal flux constants(like ERC20 token balances)
+    function _refreshInternalFluxAccounting() internal virtual;
+
+    /// @notice Refresh external flux constants(like Dex positions)
+    function _refreshExternalFluxAccounting() internal virtual;
+
+    function _totalAssets(uint256 exchangeRate)
+        internal
+        view
+        virtual
+        returns (uint256 token0Assets, uint256 token1Assets);
+}
---
+++ src/managers/UniswapV4FluxManager.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {FluxManager} from "src/FluxManager.sol";
+
+contract UniswapV4FluxManager is FluxManager {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         STRUCT                             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    struct PositionData {
+        uint128 liquidity;
+        int24 tickLower;
+        int24 tickUpper;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         STATE                              */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+    uint128 internal token0Balance;
+    uint128 internal token1Balance;
+
+    uint256[] internal trackedPositions;
+
+    PositionData[] internal trackedPositionData;
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       IMMUTABLES                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    constructor(address _owner, address _boringVault, address _token0, address _token1)
+        FluxManager(_owner, _boringVault, _token0, _token1)
+    {}
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                     FLUX FUNCTIONS                         */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @notice Refresh internal flux constants.
+    /// @dev For Uniswap V4 this is token0 and token1 contract balances
+    function _refreshInternalFluxAccounting() internal override {
+        token0Balance = uint128(token0.balanceOf(address(boringVault)));
+        token1Balance = uint128(token1.balanceOf(address(boringVault)));
+    }
+
+    /// @notice Refresh external flux constants(like Dex positions)
+    function _refreshExternalFluxAccounting() internal override {}
+
+    function _totalAssets(uint256 exchangeRate)
+        internal
+        view
+        override
+        returns (uint256 token0Assets, uint256 token1Assets)
+    {
+        return (0, 0);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                  STRATEGIST FUNCTIONS                      */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+}
